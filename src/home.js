884b659d467099e3a141bfe6acc567055646079f
Daniel Geremias <daniel.vg19@gmail.com>
2022-01-06 16:36:45 -0300

    Added Styling and filling up content on home page

--- src/home.js
+++ src/home.js
@@ -1,7 +1,20 @@
 function homePage() {
     const home = document.createElement('div');
     home.classList.add('home');
-    home.textContent = 'This is the home page!';
+
+    const heroSection = document.createElement('div');
+
+    const heroTitle = document.createElement('h2');
+    heroTitle.classList.add('hero-title');
+    heroTitle.textContent = `Let's eat at My Restaurant!`;
+    const heroText = document.createElement('p');
+    heroText.classList.add('hero-text');
+    heroText.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum purus ut ultricies interdum. Vivamus eu metus sit amet neque porta volutpat iaculis sit amet orci. Nullam sed ornare elit. Fusce in risus eu nisi finibus venenatis.`;
+
+    heroSection.appendChild(heroTitle);
+    heroSection.appendChild(heroText);
+
+    home.appendChild(heroSection)
 
     return home;
 }
